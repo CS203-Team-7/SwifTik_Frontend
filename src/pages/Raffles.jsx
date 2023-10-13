b2bc16c236e7c5fc0928cd3b07f972d588b0f4ea
clairelim <claire.lim.2022@scis.smu.edu.sg>
2023-10-13 22:18:36 +0800

    raffle page implemented

--- src/pages/Raffles.jsx
+++ src/pages/Raffles.jsx
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
 import Header from "../components/Header";
-import CurrentRaffles from "../components/CurrentRaffles";
-import PastRaffles from "../components/PastRaffles";
 
 const Raffles = () => {
     const [currentraffles] = useState([
@@ -9,7 +7,6 @@ const Raffles = () => {
             Zone: '1',
             EventDate: '03/03/2024',
             RaffleNumber: '2',
-            Type: 'raffleC',
             Status: 'Ongoing',
             Picture: 'https://upload.wikimedia.org/wikipedia/en/3/33/The_Eras_Tour_poster.jpg'
         },
@@ -17,48 +14,31 @@ const Raffles = () => {
             Zone: '2',
             EventDate: '03/06/2025',
             RaffleNumber: '6',
-            Type: 'raffleC',
             Status: 'Ongoing',
             Picture: 'https://upload.wikimedia.org/wikipedia/en/3/33/The_Eras_Tour_poster.jpg'
         },
     ]);//temporary hardcoded data
 
-    const [pastraffles] = useState([
-        {
-            Zone: '1',
-            EventDate: '03/03/2024',
-            RaffleNumber: '2',
-            Type: 'raffleP',
-            Status: 'Ongoing',
-            Picture: 'https://m.media-amazon.com/images/I/61zVGn1okXL._AC_UF894,1000_QL80_.jpg'
-        },
-        {
-            Zone: '2',
-            EventDate: '03/06/2025',
-            RaffleNumber: '6',
-            Type: 'raffleP',
-            Status: 'Ongoing',
-            Picture: 'https://m.media-amazon.com/images/I/61zVGn1okXL._AC_UF894,1000_QL80_.jpg'
-        },
-    ]);//temporary hardcoded data
 
     return (
-
         <div className="raffles">
             <Header />
             <div className="row heading">
                 <h3>Current Raffles</h3>
             </div>
             <div className="row">
-                <CurrentRaffles currentraffles={currentraffles} />
+                {currentraffles.map((raffle, index) => (
+                    <div className="raffle-container" key={index}>
+                        <img src={raffle.Picture} alt="raffle" />
+                        <div className="raffle-info"> {/*this is the info that will be displayed on the raffle card when hovering*/}
+                            <p>Zone: {raffle.Zone}</p>
+                            <p>Event Date: {raffle.EventDate}</p>
+                            <p>Raffle Number: {raffle.RaffleNumber}</p>
+                            <p>Status: {raffle.Status}</p>
+                        </div>
+                    </div>
+                ))}
             </div>
-            <div className="row heading">
-                <h3>Past Raffles</h3>
-            </div>
-            <div className="row">
-                <PastRaffles pastraffles={pastraffles} />
-            </div>
-        
         </div>
     );
 };
