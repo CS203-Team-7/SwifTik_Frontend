0963f51e0d301636613deba9f25f7922bb362cac
Ryan <rmmsia@gmail.com>
2023-11-02 16:24:42 +0800

    Added functionality for multiple buttons
    There are now as many buttons as there are zones available

--- src/pages/SeatMap.jsx
+++ src/pages/SeatMap.jsx
@@ -2,9 +2,24 @@ import React, { useState } from "react";
 import Header from "../components/Header";
 
 const SeatMap = () => {
-    const handleZoneButtonClick = () => {
+    const [availableZones] = useState([
+        {
+            Zone: 'Alpha',
+            Price: '$200'
+        },
+        {
+            Zone: 'Bravo',
+            Price: '$100'
+        },
+        {
+            Zone: 'Charlie',
+            Price: '$50'
+        },
+    ]);//temporary hardcoded data
+
+    const handleZoneButtonClick = (zone) => {
         // Define what happens when the button is clicked here
-        alert("Zone button clicked!");
+        alert(`Zone button clicked for ${zone.Zone}`);
     };
 
     return (
@@ -12,13 +27,21 @@ const SeatMap = () => {
             <Header />
             <div className="seatmap-heading">
                 <h1>Seat Map</h1>
-                
             </div>
             <div className="seatmap-container">
             <img style={{ width: 500, height: 600 }}src="https://www.sportshub.com.sg/sites/default/files/2023-07/NST-TS-seatmap-VIP-V4.jpg" alt="seatmap"/>
             </div>
-            <div className="zone-button" onClick={handleZoneButtonClick}>
-                <text>Zone</text>
+            <div className="zone-buttons">
+                {availableZones.map((zone, index) => (
+                    <button
+                        key={index}
+                        className="zone-button"
+                        onClick={() => handleZoneButtonClick(zone)}
+                    >
+                        <span className="zone-name">{zone.Zone}</span>
+                        <br></br><span className="price">{zone.Price}</span>
+                    </button>
+                ))}
             </div>
         </div>
     );
