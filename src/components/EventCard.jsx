fbb0d6b58b86f9b467c5b127f97efefe5ed3bf2a
Dhruv Benegal <benegaldhruv2312@gmail.com>
2023-11-05 15:48:57 +0800

    Linked Events to seatmap

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
 import '../css/poster.css'
+import { Link } from 'react-router-dom';
 
 const EventCard = ({key, event, counter}) => {
     return (
-        <div className='box'>
-            {counter % 2 === 0 || key === 0 ? 
-                <>
-                <div className='poster'></div>
-                </>  
-                :
-                <>
-                <div className='poster-2'></div>
-                </>  
-            }
-            <h1 className='name'>{counter}. {event.eventName} - {event.venue}</h1>
+        <div>
+            <Link to="/seatmap">
+                <div className='box'>
+                    {counter % 2 === 0 || key === 0 ? 
+                        <>
+                        <div className='poster'></div>
+                        </>  
+                        :
+                        <>
+                        <div className='poster-2'></div>
+                        </>  
+                    }
+                    <h1 className='name'>{counter}. {event.eventName} - {event.venue}</h1>
+                </div>
+            </Link>
         </div>
     )
 }
